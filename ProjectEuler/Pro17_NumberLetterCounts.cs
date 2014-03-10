1529d8c81f7700da4b6575308ee021bcfc21f9a3
matrixkong <matrixkongjiahui@gmail.com>
2014-03-11 00:16:18 +1100

    update version

--- ProjectEuler/Pro17_NumberLetterCounts.cs
+++ ProjectEuler/Pro17_NumberLetterCounts.cs
@@ -11,6 +11,12 @@ namespace ProjectEuler
     {
         public static void Main()
         {
+
+            const string teen = "teen"; // for 10 ~ 20.
+            const string ty = "ty"; // for 20 ~ 100.
+            const string hundred = "hundred"; // for number bigger than 100.
+            const string and = "and"; // for hundred and double digit.
+
             var array = new string[1001];
             array[0] = "";
             array[1] = "one";
@@ -23,25 +29,16 @@ namespace ProjectEuler
             array[8] = "eight";
             array[9] = "nine";
             array[10] = "ten";
-
-
             array[11] = "eleven";
             array[12] = "twelve";
-
-            const string teen = "teen";
             array[13] = "thirteen";
             array[15] = "fifteen";
             array[18] = "eighteen";
-
-            const string ty = "ty";
             array[20] = "twenty";
             array[30] = "thirty";
             array[40] = "forty";
             array[50] = "fifty";
             array[80] = "eighty";
-
-            const string hundred = "hundred";
-            const string and = "and";
             array[1000] = "onethousand";
 
             for (int i = 14; i <= 1000; i++)
@@ -51,62 +48,53 @@ namespace ProjectEuler
                     var result = new StringBuilder();
                     var singleDigit = i % 10;
                     var doubleDigit = i % 100;
-                    if (doubleDigit > 10 && doubleDigit < 20)
-                    {
-                        if (string.IsNullOrEmpty(array[doubleDigit]))
-                        {
-                            result.Append(array[singleDigit]);
-                            result.Append(teen);
-                        }
-                        else
-                        {
-                            result.Append(array[doubleDigit]);
-                        }
-                    }
-                    if (doubleDigit >= 20)
-                    {
-                        if (string.IsNullOrEmpty(array[doubleDigit]))
-                        {
 
-                            result.Append(array[singleDigit]);
-                            doubleDigit = doubleDigit - singleDigit;
-                            if (string.IsNullOrEmpty(array[doubleDigit]))
+
+                    if (string.IsNullOrEmpty(array[doubleDigit]))
+                    {
+                        result.Append(array[singleDigit]);
+                        var secondDigit = (doubleDigit - singleDigit) / 10;
+                        var tempNumber = doubleDigit - singleDigit;
+
+                        if (secondDigit >= 2)
+                        {
+                            if (string.IsNullOrEmpty(array[tempNumber]))
                             {
-                                doubleDigit = doubleDigit / 10;
-                                result.Append(array[doubleDigit]);
+                                result.Append(array[secondDigit]);
                                 result.Append(ty);
                             }
                             else
                             {
-                                result.Append(array[doubleDigit]);
+                                result.Append(array[tempNumber]);
                             }
                         }
-                        else
+                        else if (secondDigit > 0)
                         {
-                            result.Append(array[doubleDigit]);
+                            result.Append(teen);
                         }
                     }
+                    else
+                    {
+                        result.Append(array[doubleDigit]);
+                    }
+
                     if (i >= 100)
                     {
-                        var tripleDigit = i/100;
-                        //result.Append(array[singleDigit]);
-                        if (doubleDigit!=0) 
+                        var tripleDigit = i / 100;
+                        if (doubleDigit != 0)
                             result.Append(and);
-                        if (doubleDigit <= 10)
-                            result.Append(array[doubleDigit]); 
                         result.Append(array[tripleDigit]);
                         result.Append(hundred);
                     }
-                    //Console.WriteLine(result.ToString());
                     array[i] = result.ToString();
                 }
             }
             using (var file = new StreamWriter(@"C:\txt.txt"))
             {
                 foreach (string line in array)
-                { 
-                        file.WriteLine(line); 
-                } 
+                {
+                    file.WriteLine(line);
+                }
             }
             var sum = 0;
             foreach (var s in array)
