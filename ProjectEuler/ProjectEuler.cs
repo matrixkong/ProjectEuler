df75ff2552b9541ce8bf7b6c16183520d118231c
matrixkong <matrixkongjiahui@gmail.com>
2014-03-11 20:41:44 +1100

    add entry point

---
+++ ProjectEuler/ProjectEuler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class ProjectEuler
+    {
+        public static void Main()
+        {
+            Pro3LargestPrimeFactor.LargestPrimeFactor();
+            Pro4LargestPalindromeProduct.LargestPalindromeProduct();
+            Pro5SmallestMultiple.SmallestMultiple();
+            Pro6SumSquareDifference.SumSquareDifference();
+            Pro7Prime10001.Prime10001();
+            Pro8LargestProductASeries.LargestProductASeries();
+            Pro9SpecialPythagoreanTriplet.SpecialPythagoreanTriplet();
+            Pro10SummationOfPrimes.SummationOfPrimes();
+            Pro11LargestProductInAGrid.LargestProductInAGrid();
+            Pro12HighDivisibleTangularNmber.HighDivisibleTangularNmber();
+            Pro13Largesum.Largesum();
+            Pro14LongestCollatzsequence.LongestCollatzsequence();
+            Pro15LatticePaths.LatticePaths();
+            Pro17NumberLetterCounts.NumberLetterCounts(); 
+        }
+    }
+}
